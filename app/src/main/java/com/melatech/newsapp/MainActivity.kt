488d915a46b48d49cace97c5550d6f32c6128e8f
thirusatkunam <thiruandramani@googlemail.com>
2022-10-30 18:56:47 +0000

    Adding a placeholder fragment to be hosted by the main activity, in order to show the list of news when we fetch them from server.

--- app/src/main/java/com/melatech/newsapp/MainActivity.kt
+++ app/src/main/java/com/melatech/newsapp/MainActivity.kt
@@ -7,5 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // create fragment instance
+        val newsFragment : NewsFragment = NewsFragment.newInstance()
+
+
+        // check is important to prevent activity from attaching the fragment if already its attached
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, newsFragment, "news_fragment")
+                .commit()
+        }
     }
 }
