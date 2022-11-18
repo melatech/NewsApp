00ad66f412bb3b961f94359cd1f88a3ad0f65e4c
thirusatkunam <thiruandramani@googlemail.com>
2022-11-18 01:29:36 +0000

    - adding progresbar while loading news

--- app/src/main/java/com/melatech/newsapp/news/NewsDetailsFragment.kt
+++ app/src/main/java/com/melatech/newsapp/news/NewsDetailsFragment.kt
@@ -1,11 +1,13 @@
 package com.melatech.newsapp.news
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import com.melatech.newsapp.R
 
@@ -23,8 +25,21 @@ class NewsDetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val contentUrl = arguments?.getString(CONTENT_URL_KEY)
         val news_details_webview = view.findViewById<WebView>(R.id.news_details_webview)
+        val news_details_progressBar = view.findViewById<ProgressBar>(R.id.news_details_progressBar)
         news_details_webview.apply {
-            webViewClient = WebViewClient()
+            webViewClient = object : WebViewClient() {
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    super.onPageStarted(view, url, favicon)
+                }
+                override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                    view.loadUrl(url)
+                    return false
+                }
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    news_details_progressBar.visibility = View.GONE
+                }
+            }
             contentUrl?.run { loadUrl(this) }
         }
     }
