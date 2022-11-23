d81f2d89d194e2e5329cc79218e81bc036159f33
tsatkunam <t.satkunam@cmcmarkets.com>
2022-11-23 01:33:46 +0000

    force app to be in light theme mode, no night mode support

--- app/src/main/java/com/melatech/newsapp/NewsApplication.kt
+++ app/src/main/java/com/melatech/newsapp/NewsApplication.kt
@@ -1,7 +1,14 @@
 package com.melatech.newsapp
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
+
 @HiltAndroidApp
-class NewsApplication : Application()
+class NewsApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
